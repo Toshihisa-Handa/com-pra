58bac2ab74355a31d64657261e2c4defc94871c7
Toshi <waykiyoharu@gmail.com>
2020-08-05 06:39:06 +0900

    loginページ作成

---
+++ src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12">
+
+      <LoginForm/>
+
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm'
+
+export default{
+    name:'Login',
+    components:{
+        LoginForm,
+    },
+}
+
+
+
+</script>
